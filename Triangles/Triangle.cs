f3b364648bab1343905f1145d56bfc9a3558975c
xxblancxx <xxblancxx@hotmail.com>
2017-02-13 15:25:29 +0200

    First gitpush to new git

---
+++ Triangles/Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangles
+{
+    class Triangle
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        private string _type;
+
+        public string GetType()
+        {
+            // Returns whether the triangle is scalene, isosceles or equilateral
+            //scalene(ingen ens sider): No sides equal length
+            //isosceles(ligebenet): 2/3 sides equal length
+            //equilateral (ligesidet): All sides equal length
+            if (SideA == SideB && SideA == SideC)
+            { // All sides are equal
+                return "equilateral";
+            }
+            else if (SideA == SideB ||SideB == SideC || SideC == SideA)
+            { // 2 sides are equal.
+                return "isosceles";
+            }
+            else
+            { // No sides are equal
+                return "scalene";
+            }
+        }
+    }
+}
